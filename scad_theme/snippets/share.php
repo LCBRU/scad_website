d843f63e215e6de6e465bd7edfa77b844a473dc9
Richard Bramley <rabramley@gmail.com>
2024-05-15 11:13:20 +0100

    Fix base url for share

--- scad_theme/snippets/share.php
+++ scad_theme/snippets/share.php
@@ -1,4 +1,11 @@
-<div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?=base_url()?>@{url}" data-a2a-title="@{title}">
+<?php
+ 
+$parsed_url = parse_url(base_url());
+$real_base_url = $parsed_url['scheme'] . "://" . $parsed_url['host'] ;
+ 
+?>
+
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="<?=$real_base_url?>@{url}" data-a2a-title="@{title}">
     <a class="a2a_button_facebook" target="_blank"></a>
     <a class="a2a_button_twitter" target="_blank"></a>
     <a class="a2a_button_email" target="_blank"></a>
